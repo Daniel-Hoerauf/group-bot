3d6b15023c872acf09660d22f3b2acfc98be29a2
Daniel Hoerauf <dah4pe@virginia.edu>
2016-06-18 22:00:02 +0000

    Receive GroupMe messages

--- web.go
+++ web.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"os"
+	"io/ioutil"
 	"fmt"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world\n")
+	r.ParseForm()
 	fmt.Println("Content received!")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	message := string(body[:])
+	fmt.Println(message)
 }
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":80", nil)
 }
